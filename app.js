884249d7828b475d4d63c9ecddcbe9a951ec171f
AndresDeLaRosaJasso <delarosajassoandres@gmail.com>
2026-05-14 15:47:38 -0600

    Create a smooth diagonal transition

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const sorteos = document.querySelectorAll('.sorteo');
+
+/* Transicion de transitionOverlay_01 */
+const trans = () => {
+    overlay.classList.add('active');
+    setTimeout(() => {
+        overlay.classList.remove('active');
+    }, 3500);
+}
+
+const overlay = document.querySelector('.transitionOverlay_01');
+sorteos.forEach(sorteo => {
+    sorteo.addEventListener('click', trans);
+});
+
+
